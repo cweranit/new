bc4ae3f4a2daa76d9cdd6d11176ebf288e2c3e8f
cweranit <37048571+cweranit@users.noreply.github.com>
2018-03-21 22:57:35 +0300

    Update code.lua

--- code.lua
+++ code.lua
@@ -6,20 +6,19 @@ fm.setScreenColor(0xFF0000)
 if text == seturl then
 	fm.setURL("http://dl2.mp3party.net/download/2005805")
 	url = 1
-else if text == play and url == 1 then
+elseif text == play and url == 1 then
 	fm.start()
 	play = 1
-else if text == stop and play == 1 then
+elseif text == stop and play == 1 then
 	fm.stop()
 	play = 0
-else if text == volu then
+elseif text == volu then
 	fm.volUp()
-else if text == vold then
+elseif text == vold then
 	fm.volDown()
-else if text == radio_help then
+elseif text == radio_help then
 	print("seturl - Установить народную песню")
 	print("play - Старт")
 	print("stop - Стоп")
 	print("volu/vold - Прибавить/Убавить")
-end
 end
